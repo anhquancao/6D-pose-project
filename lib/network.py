8050dd1c6537559e0db0eedbf0df97d9feae7578
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-29 10:39:52 +0000

    modify architecture to use only RGB image

--- lib/network.py
+++ lib/network.py
@@ -24,6 +24,79 @@ psp_models = {
     'resnet152': lambda: PSPNet(sizes=(1, 2, 3, 6), psp_size=2048, deep_features_size=1024, backend='resnet152')
 }
 
+class PoseNetRGBOnly(nn.Module):
+    def __init__(self, num_points, num_obj):
+        super(PoseNetRGBOnly, self).__init__()
+        self.num_points = num_points
+        self.cnn = ModifiedResnet()
+        self.conv1 = torch.nn.Conv1d(32, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 256, 1)
+        self.conv4 = torch.nn.Conv1d(256, 512, 1)
+        self.conv5 = torch.nn.Conv1d(512, 1024, 1)
+
+        
+        self.conv1_r = torch.nn.Conv1d(1024, 512, 1)
+        self.conv1_t = torch.nn.Conv1d(1024, 512, 1)
+        self.conv1_c = torch.nn.Conv1d(1024, 512, 1)
+
+        self.conv2_r = torch.nn.Conv1d(512, 256, 1)
+        self.conv2_t = torch.nn.Conv1d(512, 256, 1)
+        self.conv2_c = torch.nn.Conv1d(512, 256, 1)
+
+        self.conv3_r = torch.nn.Conv1d(256, 128, 1)
+        self.conv3_t = torch.nn.Conv1d(256, 128, 1)
+        self.conv3_c = torch.nn.Conv1d(256, 128, 1)
+
+        self.conv4_r = torch.nn.Conv1d(128, num_obj*4, 1) #quaternion
+        self.conv4_t = torch.nn.Conv1d(128, num_obj*3, 1) #translation
+        self.conv4_c = torch.nn.Conv1d(128, num_obj*1, 1) #confidence
+
+        self.num_obj = num_obj
+    
+    def forward(self, img, choose, obj):
+        out_img = self.cnn(img)
+        
+        bs, di, _, _ = out_img.size()
+        
+        emb = out_img.view(bs, di, -1)
+        choose = choose.repeat(1, di, 1)
+        emb = torch.gather(emb, 2, choose).contiguous()
+        
+        emb = self.conv1(emb)
+        emb = self.conv2(emb)
+        emb = self.conv3(emb)
+        emb = self.conv4(emb)
+        emb = self.conv5(emb)
+        
+        rx = F.relu(self.conv1_r(emb))
+        tx = F.relu(self.conv1_t(emb))
+        cx = F.relu(self.conv1_c(emb))      
+
+        rx = F.relu(self.conv2_r(rx))
+        tx = F.relu(self.conv2_t(tx))
+        cx = F.relu(self.conv2_c(cx))
+
+        rx = F.relu(self.conv3_r(rx))
+        tx = F.relu(self.conv3_t(tx))
+        cx = F.relu(self.conv3_c(cx))
+
+        rx = self.conv4_r(rx).view(bs, self.num_obj, 4, self.num_points)
+        tx = self.conv4_t(tx).view(bs, self.num_obj, 3, self.num_points)
+        cx = torch.sigmoid(self.conv4_c(cx)).view(bs, self.num_obj, 1, self.num_points)
+        
+        b = 0
+        out_rx = torch.index_select(rx[b], 0, obj[b])
+        out_tx = torch.index_select(tx[b], 0, obj[b])
+        out_cx = torch.index_select(cx[b], 0, obj[b])
+        
+        out_rx = out_rx.contiguous().transpose(2, 1).contiguous()
+        out_cx = out_cx.contiguous().transpose(2, 1).contiguous()
+        out_tx = out_tx.contiguous().transpose(2, 1).contiguous()
+        
+        return out_rx, out_tx, out_cx, emb.detach()
+        
+
 class ModifiedResnet(nn.Module):
 
     def __init__(self, usegpu=True):
@@ -50,6 +123,7 @@ class PoseNetFeat(nn.Module):
 
         self.ap1 = torch.nn.AvgPool1d(num_points)
         self.num_points = num_points
+        
     def forward(self, x, emb):
         x = F.relu(self.conv1(x))
         emb = F.relu(self.e_conv1(emb))
@@ -67,6 +141,10 @@ class PoseNetFeat(nn.Module):
         ap_x = ap_x.view(-1, 1024, 1).repeat(1, 1, self.num_points)
         return torch.cat([pointfeat_1, pointfeat_2, ap_x], 1) #128 + 256 + 1024
 
+    
+
+        
+    
 class PoseNet(nn.Module):
     def __init__(self, num_points, num_obj):
         super(PoseNet, self).__init__()

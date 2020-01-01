08338bf309b0986aeb639f026002dd0f68f1179c
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-01-01 20:54:58 +0000

    save

--- lib/loss_depth.py
+++ lib/loss_depth.py
@@ -6,24 +6,34 @@ import numpy as np
 import torch.nn as nn
 import random
 
-def log_diff(log_yi, yi_true):
-    return log_yi - torch.log(yi_true)
 
-class LossDepth(_Loss):
+class LogL2(_Loss):
 
     def __init__(self):
-        super(LossDepth, self).__init__(True)
+        super(LogL2, self).__init__(True)
     
     def forward(self, pred_log_depth, true_depth):
         mask_depth = true_depth.eq(0.).float()
+        log_true_depth = torch.log(true_depth + mask_depth)
         
-        di = log_diff(pred_log_depth, true_depth + mask_depth)
-        
-        di *= (1.0 - mask_depth)
-        
-        sum_di_square = torch.sum(di ** 2)
-        square_sum_di = torch.sum(di) ** 2
+        diff = pred_log_depth - log_true_depth
+        diff *= (1.0 - mask_depth)
         
         n = torch.sum(1 - mask_depth)
         
-        return 1/n * sum_di_square - 0.5 / (n * n) * square_sum_di
+        return torch.sqrt(torch.sum(diff ** 2)) / n
+
+class L2(_Loss):
+    def __init__(self):
+        super(L2, self).__init__(True)
+        
+    def forward(self, pred_log_depth, true_depth):
+        mask_depth = true_depth.eq(0.).float()
+        pred_depth = torch.exp(pred_log_depth)
+        
+        diff  = (pred_depth - true_depth) 
+        diff *= (1.0 - mask_depth)
+        
+        n = torch.sum(1 - mask_depth)
+        
+        return torch.sqrt(torch.sum(diff ** 2)) / n
